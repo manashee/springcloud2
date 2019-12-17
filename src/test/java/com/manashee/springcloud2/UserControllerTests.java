bf60048dff58eaede27061bebf2cf06fb09a4ca6
ashok.kumar@bdb.ai <manashee@gmail.com>
2019-12-17 11:18:10 +0530

    Added UserController and Tests

---
+++ src/test/java/com/manashee/springcloud2/UserControllerTests.java
@@ -0,0 +1,31 @@
+package com.manashee.springcloud2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.hamcrest.CoreMatchers.containsString;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getUsers() throws Exception {
+        this.mockMvc.perform(get("/users")).andDo(print()).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string(containsString("Ashok")));
+    }
+
+}
