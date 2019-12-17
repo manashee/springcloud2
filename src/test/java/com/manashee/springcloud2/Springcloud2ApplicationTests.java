fb62e6d5f28874c5f00a02ae8a657eafd27f6695
ashok.kumar@bdb.ai <manashee@gmail.com>
2019-12-17 10:58:51 +0530

    pom

---
+++ src/test/java/com/manashee/springcloud2/Springcloud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manashee.springcloud2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcloud2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
